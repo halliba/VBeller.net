fb40e90867531207c708dfe9c6f1fa7688e9d6e1
Valentin <valentin.beller@gmail.com>
2017-07-03 12:17:59 +0200

    added VBeller.Wpf and VBeller.Wpf.Controls with content

---
+++ src/VBeller.Wpf.Controls/Calendar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VBeller.Wpf.Controls
+{
+    public class Calendar : System.Windows.Controls.Calendar
+    {
+        public static readonly DependencyProperty InvertedScrollProperty;
+        public static readonly DependencyProperty IsScrollEnabledProperty;
+
+        public bool InvertedScroll
+        {
+            get => (bool)GetValue(InvertedScrollProperty);
+            set => SetValue(InvertedScrollProperty, value);
+        }
+
+        public bool IsScrollEnabled
+        {
+            get => (bool)GetValue(IsScrollEnabledProperty);
+            set => SetValue(IsScrollEnabledProperty, value);
+        }
+
+        public Calendar()
+        {
+            MouseWheel += OnMouseWheel;
+        }
+
+        static Calendar()
+        {
+            InvertedScrollProperty = DependencyProperty.Register(nameof(InvertedScroll),
+                typeof(bool), typeof(Calendar), new FrameworkPropertyMetadata(false));
+            IsScrollEnabledProperty = DependencyProperty.Register(nameof(IsScrollEnabled),
+                typeof(bool), typeof(Calendar), new FrameworkPropertyMetadata(false));
+        }
+
+        private void OnMouseWheel(object sender, MouseWheelEventArgs mouseWheelEventArgs)
+        {
+            if (!IsScrollEnabled) return;
+
+            var up = InvertedScroll ? -1 : 1;
+            var down = InvertedScroll ? 1 : -1;
+
+            var delta = mouseWheelEventArgs.Delta;
+            switch (DisplayMode)
+            {
+                case CalendarMode.Month:
+                    DisplayDate = delta < 0 ? DisplayDate.AddMonths(up) : DisplayDate.AddMonths(down);
+                    break;
+                case CalendarMode.Year:
+                    DisplayDate = delta < 0 ? DisplayDate.AddYears(up) : DisplayDate.AddYears(down);
+                    break;
+                case CalendarMode.Decade:
+                    DisplayDate = delta < 0 ? DisplayDate.AddYears(up * 10) : DisplayDate.AddYears(down * 10);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ src/VBeller.Wpf/Input/DelegateCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace VBeller.Wpf.Input
+{
+    /// <summary>
+    /// Represents a <see cref="ICommand"/> implementation, that can be used vi delegates and lamda expressions.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// Backup field for the <see cref="ICommand.CanExecute"/> <see cref="Predicate{T}"/>.
+        /// </summary>
+        private readonly Predicate<object> _canExecute;
+
+        /// <summary>+
+        /// Backup field for the <see cref="ICommand.Execute"/> <see cref="Action{T}"/>.
+        /// </summary>
+        private readonly Action<object> _execute;
+
+        /// <summary>
+        /// Gets executed, when the <see cref="CanExecute"/> result probably changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Creates a new Instance of the <see cref="DelegateCommand"/> class with the given <see cref="ICommand.Execute"/> <see cref="Action{T}"/>
+        /// and optionally a <see cref="ICommand.CanExecute"/> <see cref="Predicate{T}"/>.
+        /// </summary>
+        /// <param name="execute">The <see cref="Action"/>, to be exeucted on <see cref="Execute"/>.</param>
+        /// <param name="canExecute">The <see cref="Predicate{T}"/>, executed in <see cref="CanExecute"/> that indicates if the command
+        /// is able to execute.</param>
+        public DelegateCommand(Action<object> execute,
+            Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged"/> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, null);
+        }
+
+        /// <summary>
+        /// Executes the <see cref="ICommand.CanExecute"/> <see cref="Predicate{T}"/> and returns it's result.
+        /// </summary>
+        /// <param name="parameter">The command parameter.</param>
+        /// <returns>True, if the command is able to execute.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Executes the <see cref="ICommand.Execute"/> <see cref="Action{T}"/> with the given parameter.
+        /// </summary>
+        /// <param name="parameter">The command parameter.</param>
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
